830887cb803b6ba6cdb3d7adc1bdb76d06aa1643
flowMemory <772675479@qq.com>
2020-06-19 17:56:25 +0800

    add catch exception

--- project1.0/api/v1/book.js
+++ project1.0/api/v1/book.js
@@ -1,8 +1,12 @@
 const Router = require('koa-router');
+const {HttpException, ParameterException} = require('../../core/http-exception');
 const router = new Router();
-console.log(router)
 
 router.get('/v1/api/book', (ctx, next) => {
+    if(true) {
+        const error = new HttpException('not book',999,404)
+        throw error
+    }
     console.log(ctx.path)
 });
 
--- project1.0/app.js
+++ project1.0/app.js
@@ -1,6 +1,11 @@
 const Koa = require('koa');
 const InitManager = require('./core/init')
+const catchError = require('./middlewares/exception')
 const app = new Koa();
+
+
+// 先启用全局监听异常方法
+app.use(catchError)
 
 // 调用静态方法，初始化路由注册
 InitManager.initCore(app)
