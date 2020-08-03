0eeeff9f9aae1410c1616cf768144fa5f9e76a44
flowMemory <zhengtengfei@styn.cn>
2020-08-03 22:49:33 +0800

    添加latest接口

---
+++ project1.0/app/api/v1/classic.js
@@ -0,0 +1,18 @@
+const Router = require('koa-router');
+const {PositiveIntegerValidator} = require('../../validators/validator');
+
+const router = new Router({
+    prefix: '/v1/api/book'
+})
+
+router.get('/latest', async (ctx, next) => {
+    if(true) {
+        const v = await new PositiveIntegerValidator().validate(ctx)
+        const id = v.get("query.id")
+        console.log(id)
+        ctx.body = id
+    }
+    console.log(ctx.path)
+});
+
+module.exports = router
